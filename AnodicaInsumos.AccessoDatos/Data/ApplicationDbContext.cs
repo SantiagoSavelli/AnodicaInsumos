554d39ef0e63bcbfc8574153c0c68e2c9232bd8a
santi <santi@NOTEBOOKSAVELLI>
2026-02-26 13:01:09 -0300

    Agregar archivos de proyecto.

---
+++ AnodicaInsumos.AccessoDatos/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AnodicaInsumos.AccessoDatos.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) 
+            : base(options) 
+        { 
+        
+        }
+    }
+}
---
+++ AnodicaInsumos.AccessoDatos/Data/Repository/IRepository/IContenedorTrabajo.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AnodicaInsumos.AccessoDatos.Data.Repository.IRepository
+{
+    internal class IContenedorTrabajo
+    {
+    }
+}
